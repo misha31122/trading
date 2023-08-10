95942c7530dbd5978b2bb53c01c23e720171fda4
VTB4071405@region.vtb.ru <zhiharevmu@vtb.ru>
2023-08-11 00:01:03 +0300

    initial commit

---
+++ trading-server/src/main/java/com/trading/home/TradingHomeApplication.java
@@ -0,0 +1,13 @@
+package com.trading.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingHomeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TradingHomeApplication.class, args);
+  }
+
+}
