348bb494392c8b18ac9160238a7f034ab8745ac9
VTB4071405@region.vtb.ru <zhiharevmu@vtb.ru>
2023-09-10 23:47:33 +0300

    TRADINGBOT-1 add integration test with tinkoff services

--- src/main/java/ru/tinkoff/trade/config/TinkoffApiConfig.java
+++ src/main/java/ru/tinkoff/trade/config/TinkoffApiConfig.java
@@ -1,5 +1,19 @@
 package ru.tinkoff.trade.config;
 
+import static java.util.Collections.singletonList;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.HttpClient;
@@ -11,6 +25,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import ru.tinkoff.trade.invest.api.InstrumentsServiceApi;
 import ru.tinkoff.trade.invest.api.MarketDataServiceApi;
@@ -34,6 +49,7 @@ public class TinkoffApiConfig {
   @Bean("tinkoffApiRestTemplate")
   public RestTemplate tinkoffApiRestTemplate() {
     RestTemplate restTemplate = prepareBaseRestTemplate();
+    restTemplate.setMessageConverters(singletonList(prepareEpaSpecificMessageConverter()));
     return restTemplate;
   }
 
@@ -168,6 +184,36 @@ public class TinkoffApiConfig {
         httpClient);
 
     return new RestTemplate(requestFactory);
+  }
+
+  private MappingJackson2HttpMessageConverter prepareEpaSpecificMessageConverter() {
+    var objectMapper = new ObjectMapper()
+        .registerModule(new JavaTimeModule()
+            .addSerializer(
+                OffsetDateTime.class, new OffsetDateTimeSerializer())
+            .addDeserializer(OffsetDateTime.class, new OffsetDateTimeDeserializer()));
+    objectMapper.setSerializationInclusion(Include.NON_NULL);
+    var converter = new MappingJackson2HttpMessageConverter();
+    converter.setObjectMapper(objectMapper);
+    return converter;
+  }
+
+  private static class OffsetDateTimeDeserializer extends JsonDeserializer<OffsetDateTime> {
+
+    @Override
+    public OffsetDateTime deserialize(JsonParser parser, DeserializationContext context)
+        throws IOException {
+      return OffsetDateTime.parse(parser.getText(), DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+    }
+  }
+
+  private static class OffsetDateTimeSerializer extends JsonSerializer<OffsetDateTime> {
+
+    @Override
+    public void serialize(OffsetDateTime value, JsonGenerator gen, SerializerProvider serializers)
+        throws IOException {
+      gen.writeString(DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(value));
+    }
   }
 
 }
