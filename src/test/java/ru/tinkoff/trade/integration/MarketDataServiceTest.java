5d6c561909d8bf6ca0e092e982ba06c5e5fd0766
misha31122 <50743077+misha31122@users.noreply.github.com>
2024-02-29 16:18:02 +0300

    TRADINGBOT-2 Сделал таблицы для хранения свечей, + добавил настройки liquebase (#3)
    Co-authored-by: VTB4071405@region.vtb.ru <Mail17@list.ru>

--- src/test/java/ru/tinkoff/trade/integration/MarketDataServiceTest.java
+++ src/test/java/ru/tinkoff/trade/integration/MarketDataServiceTest.java
@@ -1,15 +1,14 @@
 package ru.tinkoff.trade.integration;
 
-import static java.time.ZoneOffset.UTC;
-import static ru.tinkoff.trade.invest.dto.V1CandleInterval.HOUR;
+import static org.ta4j.core.num.NaN.NaN;
+import static ru.tinkoff.trade.invest.dto.V1CandleInterval._4_HOUR;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,8 +18,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.ta4j.core.BarSeries;
 import org.ta4j.core.BaseBarSeriesBuilder;
-import org.ta4j.core.indicators.EMAIndicator;
-import org.ta4j.core.indicators.SMAIndicator;
+import org.ta4j.core.indicators.MACDIndicator;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import ru.tinkoff.trade.invest.api.MarketDataServiceApi;
 import ru.tinkoff.trade.invest.dto.V1GetCandlesRequest;
@@ -37,11 +35,16 @@ public class MarketDataServiceTest {
 
   @Test
   public void getCandlesByMonth() {
+    String fromDateTime = "2024-01-26T04:00:00+00:00";
+    String toDateTime = "2024-02-22T12:00:00+00:00";
+    DateTimeFormatter formatter = DateTimeFormatter.ISO_ZONED_DATE_TIME;
+    ZonedDateTime zonedDateTimeFrom = ZonedDateTime.parse(fromDateTime, formatter);
+    ZonedDateTime zonedDateTimeTo = ZonedDateTime.parse(toDateTime, formatter);
     V1GetCandlesRequest request = new V1GetCandlesRequest()
         .figi("TCS00A105EX7")
-        .from(LocalDate.now().atStartOfDay().minusDays(6).atOffset(UTC))
-        .to(LocalDate.now().atTime(LocalTime.MAX).minusDays(6).atOffset(UTC))
-        .interval(HOUR)
+        .from(zonedDateTimeFrom.toOffsetDateTime())
+        .to(zonedDateTimeTo.toOffsetDateTime())
+        .interval(_4_HOUR)
         .instrumentId("TCS00A105EX7");
     V1GetCandlesResponse response = marketDataServiceApi.marketDataServiceGetCandles(request);
     List<V1HistoricCandle> candles = response.getCandles();
@@ -58,10 +61,8 @@ public class MarketDataServiceTest {
               Double.valueOf(candle.getVolume())));
 
     ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
-    EMAIndicator shortEma = new EMAIndicator(closePrice, 9);
-    System.out.println("5-bars-EMA value at the 4th index: " + shortEma.getValue(4).doubleValue());
-
-
+    MACDIndicator macdIndicator = new MACDIndicator(closePrice, 8, 17);
+    System.out.println(Optional.of(macdIndicator).map(indicator -> indicator.getValue(0)).orElse(NaN));
   }
 
 }
